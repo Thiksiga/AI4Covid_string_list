fc1de6c12a395aecb1d29ccacccfb7fe82719ca7
Thiksiga <thiksigar@gmail.com>
2023-08-27 13:46:05 +0530

    String list day wise and dataframe with timestamp

--- File_date extraction.py
+++ File_date extraction.py
@@ -8,7 +8,8 @@ import csv
 f=f"C:\AI4Covid\DATA\clusters\eps1minPTS5\Avishka_Perera.xlsx_ClusterData.csv"
 # for f in csv_files_num:
 df=pd.read_csv(f)
-# print(df.Date)
+f_name=f.split("\\")[-1]
+print(f_name)
 
 # Extract unique dates
 df_conv=pd.to_datetime(df['Date'])
@@ -16,11 +17,22 @@ unique_dates = df_conv.dt.date.unique()
 
 # print(unique_dates)
 # print(type(unique_dates))
+summary=pd.DataFrame(columns=['cluster_num'])
+
+cluster=df['Cluster_DBSCAN_0.0001_5']
 
 for i in df.Date:
     I=pd.to_datetime(i)
+
     for j in unique_dates:
         if(j==I.date()):
-            print('Same same same same same')
+            print('Same',i)
+            summary = summary._append({'cluster_num':cluster},ignore_index=True)
 
+    print(summary)
+
+    mapfolder_path = f"C:\AI4Covid\DATA\clusters\eps1minPTS5\date_sep"
+    mapfile_path = f"{mapfolder_path}\{i}.csv"
+
+    summary.to_csv(mapfile_path, index=False)
 
--- String_trajectory.py
+++ String_trajectory.py
@@ -1,61 +1,90 @@
-# import necessary libraries
+# IMPORT NECESSARY LIBRARIES
 import pandas as pd
 import os
 import glob
 import re
 
+def Create_DataFrame(key_,val_):
+    data_ = {'key_': key_,
+             'value_': val_}
+    data_df = pd.DataFrame(data_)
+    print(data_df)
+    return data_df
 
-#Variable initializtion
-string_list=[]
 
-
-# use glob to get all the csv files
-# in the folder
-path_num = "C:\AI4Covid\DATA\clusters\eps1minPTS5"   #os.getcwd() //MANUALLY SET FOLDER PATH//
+path_num = "C:\AI4Covid\DATA\clusters\eps1minPTS5"   #CLUSTER DATA FOLDER PATH
 csv_files_num = glob.glob(os.path.join(path_num, "*.csv"))
 
-path_catalog = "C:\AI4Covid\DATA\catalogues\csv_files"   #os.getcwd() //MANUALLY SET FOLDER PATH//
+path_catalog = "C:\AI4Covid\DATA\catalogues\csv_files\eps1minPTS5"   #CATALOG FOLDER PATH
 csv_files_catalog = glob.glob(os.path.join(path_catalog, "*.csv"))
 
-# loop over the list of csv files
+# LOOP OVER THE LIST OF CSV FILES
 for f in csv_files_num:
     # read the csv file
     df = pd.read_csv(f)
 
     f_name=f.split("\\")[-1]
 
-
-    numbered_cluster = df["Cluster_DBSCAN_0.0001_5"].values.tolist()        #CLUSTER NUMBER COLUMN NAME
+    # CLUSTER NUMBER COLUMN NAME
+    numbered_cluster = df["Cluster_DBSCAN_0.0001_5"].values.tolist()
 
 
     for fl in csv_files_catalog:
-        fl_name=fl.split("\\")[-1]  #CATALOG FILE NAME SPLIT
-        epsminPTS=fl_name.split("_")[-1]    #EPSMINPTS VALUE
+        fl_name=fl.split("\\")[-1]                  #CATALOG FILE NAME SPLIT
+        epsminPTS=fl_name.split("_")[-1]            #EPSMINPTS VALUE
 
         csv_name=f_name.split("_")[0]
         catalog_name = fl_name.split("_")[0]
 
+        # EXTRACT UNIQUE DATES
+        df_conv = pd.to_datetime(df['Date'])
+        unique_dates = df_conv.dt.date.unique()
+
         if(csv_name==catalog_name):
             numbered_cluster_copy=[]
+            TIME_STAMP=[]
+            dic_val=[]
+            data_dict=[]
             print(csv_name,"=",catalog_name,"----->",epsminPTS)
             df_tag=pd.read_csv(fl,header=None)
             key=df_tag.iloc[:, [0]].values.tolist()
             tag= df_tag.iloc[:, [1]].values.tolist()
 
-            for i in range(len(numbered_cluster)):
-                # print(i,"val:=",numbered_cluster[i])
-                if(numbered_cluster[i]==-1):
-                    # print("O_L")
-                    numbered_cluster_copy.append("O_L")
+            date_ = df['Date'][0]
+            print(date_)
 
-                else:
+            for i in range(len(numbered_cluster)):
+                if(df['Date'][i]!=date_):
+
+                    #DATAFRAME FORMAT OUTPUT
+                    Create_DataFrame(TIME_STAMP,numbered_cluster_copy)
+
+
+                    # SEPERATE LISTS OUTPUT
+                    print(numbered_cluster_copy)
+                    print(TIME_STAMP)
+                    date_=df['Date'][i]
+                    print(date_)
+
+                    #CLEARING THE LISTS
+                    numbered_cluster_copy.clear()
+                    TIME_STAMP.clear()
+
+                if(numbered_cluster[i]==-1):    #Replacing cluster number -1 (Outliers) with O_L
+                    numbered_cluster_copy.append("O_L")
+                    TIME_STAMP.append(df.Time[i])
+
+                else:                           #If not an outlier; replacing with the relavent tags
                     end = len(key)
                     for j in range(end):
-                        if(numbered_cluster[i]==key[j][0]):
-                            # print("tag=",tag[j-1][0])
-                            numbered_cluster_copy.append(tag[j][0])
-                            # print(numbered_cluster_copy)
 
+                        if(numbered_cluster[i]==key[j][0]):
+                            numbered_cluster_copy.append(tag[j][0])
+                            TIME_STAMP.append(df.Time[i])
+
+            Create_DataFrame(TIME_STAMP, numbered_cluster_copy)         #DATAFRAME OUTPUT
+            print(TIME_STAMP)                                           #LIST OUTPUT
             print(numbered_cluster_copy)
+
 
 
